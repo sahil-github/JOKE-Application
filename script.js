8b90d2fb928bcbb7c44d4d72b2b032406dce3e54
sahil Lawankar <138278352+sahil-github@users.noreply.github.com>
2025-09-10 12:43:18 +0530

    Change Css and Url resticted explicit content

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const JOKE_API_URL="https://v2.jokeapi.dev/joke/Programming";
+const JOKE_API_URL="https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
 function Joke(){
-    fetch('https://v2.jokeapi.dev/joke/Programming')
+    fetch('https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit')
     .then(response=>response.json())
     .then(data =>{
-        console.log(data)
+        //console.log(data)
         if (data.type === 'single') {
         document.getElementById('joke').innerHTML= data.joke;
         } else {
-         document.getElementById("joke").innerHTML = '${data.setup}<br> ${data.delivery}';
+         document.getElementById("joke").innerHTML = `${data.setup}<br> ${data.delivery}`;
     }
 })
 };
